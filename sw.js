3e083f2fb52c6864cf116d2da709e0ba3d0b60ff
Lixia Zhang <ithilien525@gmail.com>
2019-08-25 19:23:11 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -60,8 +60,13 @@ self.addEventListener('activate', event => {
  */
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+        caches.match(event.request)
+        .then(response => {
+            if(response){
+                return response;
+            } else {
+                return fetch(event.request);
+            }
         })
     );
 });
