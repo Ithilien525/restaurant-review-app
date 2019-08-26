79ece0a2b561bbd47855ba911a170afc7e86021a
Lixia Zhang <ithilien525@gmail.com>
2019-08-25 18:30:43 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Mobile Web Specialist Certification Course
+---
+
+## How to run the project
+
+- To run the project, download or clone the repository to your computer
+
+`$ git clone github.com/Ithilien525/restaurant-review-app/`
+
+- Download and install **python**
+- Open terminal and update project's directory
+- Initiate server 
+
+`python3 -m http.server`
+
+- Visit localhost **http://localhost:8000**
+
+### How to use the application
+
+- On home page, location of restaurants are marked on the Google map
+- Click **View details** on each restaurant's info box to learn more
+- On restaurant details page, click **Home** in the breadcrumb to go back to home page
+
+## Credits:
+
+- Starter code by [**Udacity**](https://github.com/udacity/mws-restaurant-stage-1)
+- [Google maps API](https://developers.google.com/maps/documentation/javascript/get-api-key)
---
+++ sw.js
@@ -0,0 +1,67 @@
+let staticCacheName = 'restaurant-cache';
+let urlsToCache = [
+  './',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './js/dbhelper.js',
+  './js/sw_registration.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+];
+
+/**
+ * Install service worker
+ */
+self.addEventListener('install', event => {
+    console.log('Service worker caching');
+	event.waitUntil(
+		caches.open(staticCacheName).then(cache => {
+            return cache.addAll(urlsToCache);
+        }).catch(error => {
+            console.log(error);
+        })
+    );
+});
+
+
+/**
+ * Activate service worker
+ */
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => {
+                    return cacheName.startsWith('restaurant-') &&
+                           cacheName != staticCacheName;
+                }).map(cacheName => {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+
+/**
+ * Fetch offline content
+ */
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
